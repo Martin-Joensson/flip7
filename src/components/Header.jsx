7eaf7269c0a8b8fea03b5b504c32d95bcac53e53
Martin Jönsson <martin.t.joensson@gmail.com>
2026-02-02 20:38:53 +0100

    Added show tooltip when clicked for better ux on mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,26 +3,36 @@ import logo from "../assets/flip7logo.webp";
 import { useSettingStore } from "../stores/settingsStore";
 import { CapitalizeFirstLetter } from "./CapitalizeFirstLetter";
 import { useGameStore } from "../stores/gameStore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Confetti } from "./Confetti";
 import { ToggleSwitch } from "./ToggleSwitch";
 import { ThemeToggle } from "./ThemeToggle";
 
 export const Header = () => {
-  const viewMode = useSettingStore((state) => state.viewMode);
   const hideHistory = useSettingStore((state) => state.hideHistory);
   const setHideHistory = useSettingStore((state) => state.setHideHistory);
 
-  const handleHistory = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const handleToggle = () => {
     setHideHistory(!hideHistory);
+    setShowTooltip(true); // Show tooltip on toggle
   };
+
+  // Hide tooltip after 2 seconds
+  useEffect(() => {
+    if (!showTooltip) return;
+    const timer = setTimeout(() => setShowTooltip(false), 2000);
+    return () => clearTimeout(timer);
+  }, [showTooltip]);
 
   return (
     <div className="flex justify-between p-2">
       <NavLink to="/">
         <img className="w-14" src={logo} />
       </NavLink>
-      <div className="flex items-center text-right">
+
+      <div className="relative flex items-center">
         <ThemeToggle />
 
         <div className="relative flex self-center group">
@@ -32,9 +42,19 @@ export const Header = () => {
             pip2="bg-flip7-dark"
             border="true"
             checked={!hideHistory}
-            onChange={() => setHideHistory(!hideHistory)}
+            onChange={handleToggle}
           />
-          <div className="absolute right-1/2 top-full mb-2 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-700 text-white text-xs rounded px-2 py-1 whitespace-nowrap z-50">
+
+          {/* Tooltip */}
+          <div
+            className={`
+              absolute right-1/2 top-full mb-2 bg-gray-700 text-white text-xs rounded px-2 py-1 whitespace-nowrap z-50
+              transition-opacity
+              ${showTooltip || "opacity-0"} 
+              ${showTooltip ? "opacity-100" : ""}
+              group-hover:opacity-100
+            `}
+          >
             {hideHistory ? "Hiding rounds" : "Showing rounds"}
           </div>
         </div>
